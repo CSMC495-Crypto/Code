0551dab334cc98b2810ab0179b0edb77c83bd714
grantsweeney2 <micronianacemsx@gmail.com>
2016-01-29 11:18:25 -0500

    Test creating a change
    Testing

---
+++ TestCodePractice.java
@@ -0,0 +1,19 @@
+package testcodepractice;
+
+/**
+ * File: TestCodePractice.java
+ * Author: Grant Sweeney
+ * Date: Jun 3, 2015
+ * Course: 
+ * Purpose: Open code for tests
+ */
+
+ 
+public class TestCodePractice {
+    
+    public static void main(String args []) {
+        
+    } // end main
+
+ 
+ } // end class
