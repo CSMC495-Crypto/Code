1eff04e5f9e72c506b1134745fc74a00919eab5d
grantsweeney2 <micronianacemsx@gmail.com>
2016-01-29 12:13:22 -0500

    GitTutorial
    This code is to show how to use github

---
+++ GitTutorial.java
@@ -0,0 +1,23 @@
+package gittutorial;
+
+/*
+ * File: GitTutorial.java
+ * Date: Jan 29, 2016
+ * Course: 
+ * Assignment: 
+ * Java Platform
+ */
+
+/**
+ *
+ *
+ * @author gsweeney
+ */
+
+public class GitTutorial {
+
+    public static void main(String[] args) {
+        
+    } //end main
+
+} //end class
