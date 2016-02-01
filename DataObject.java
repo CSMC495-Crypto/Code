0bd32591ad810d1a57b5fc71b8149b69b93e3471
tlj1899 <tlj1899@gmail.com>
2016-02-01 17:22:42 -0500

    Add comments

--- DataObject.java
+++ DataObject.java
@@ -1,5 +1,8 @@
 package client;
 
+// object to store encrypted data and encryption key
+// to be send between communication parties to enable encrypted communications
+
 public class DataObject implements java.io.Serializable {
     
     byte[] encryptedData;
