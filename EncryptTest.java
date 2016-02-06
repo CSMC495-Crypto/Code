22e6a9348a32e48dd6af224ff6ef71d108247531
okozlova <lasignorafortuna@gmail.com>
2016-02-06 17:23:31 -0500

    Update EncryptTest.java

--- EncryptTest.java
+++ EncryptTest.java
@@ -7,11 +7,19 @@ public class EncryptTest {
 	String string = "Encrypting this data";
 	Encrypt encryptObj = new Encrypt (string.getBytes());
 
+	//this test unit makes sure that plain text passed to method encryptData 
+	//gets encrypted. It also makes sure that returned object is of DataObject class
+	
 	@Test
 	public void testEncryptData() {
+		
+		DataObject data = encryptObj.encryptData();
+		
 		System.out.print(new String("Initial data to encrypt: "+string));
-		System.out.print(new String("\nResult of encryption: "+new String(encryptObj.encryptData().encryptedData)));
+		System.out.print(new String("\nResult of encryption: "+new String(data.encryptedData)));
 		assertTrue(!string.equals(new String(encryptObj.encryptData().encryptedData)));
+		assertTrue(data.getClass().getName().equals("DataObject"));
 	}
 
 }
+
