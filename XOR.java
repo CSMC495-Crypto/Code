a22684ea4c39fd9cc8b2bcbc06ef04e7aff5f61e
okozlova <lasignorafortuna@gmail.com>
2016-02-14 09:49:46 -0500

    Updated on 02/14 to match Cryptography interface
    //Changes:
    // 1. method name changed from encryptData to encrypt to match interface
    // 2. Constructor that initializes a value to String plainText removed
    // 3. String plainText is passed as argument to encrypt method to match interface
    // 4. method name decryptData is changed to 'decrypt' to match interface
    //   added JavaDoc for the class and methods

--- XOR.java
+++ XOR.java
@@ -1,19 +1,40 @@
-// simple cryptographic program which encrypts and decrypts data
-// by XORing a binary key to each byte of data
 
-package client;
+/**
+ * This class represents simple cryptographic program 
+ * which encrypts and decrypts data
+ * by XORing a binary key to each byte of data.
+ * @author Jonathan Wojack
+ * @project Bank Encryption Application
+ * @course CSMC 495
+ * @date created 02/02/2016
+ * @updated on 02/14/2016 by Olga Kazlova 
+ */
+
+//Changes:
+// 1. method name changed from encryptData to encrypt to match interface
+// 2. Constructor that initializes a value to String plainText removed
+// 3. String plainText is passed as argument to encrypt method to match interface
+// 4. method name decryptData is changed to 'decrypt' to match interface
+//   added JavaDoc for the class and methods
 
 public class XOR {
     
-    public static String plainText;
+   // public static String plainText;
     public static Base64 base64 = new Base64();
-    public XOR(String plainText) {
+    //public XOR(String plainText) {
         
-        this.plainText = plainText;
+   //     this.plainText = plainText;
         
-    }
+   // }
     
-    public DataObject encryptData() {  
+    /**
+     * This method encrypts plain text passed as an argument 
+     * by XORing a binary key to each byte of the data 
+     * @param plainText String
+     * @return DataObject decodedData
+     */
+    
+    public DataObject encrypt(String plainText) {  
         
        
         
@@ -22,13 +43,19 @@ public class XOR {
         Encrypt encrypt = new Encrypt(plainText64);
         
         DataObject encryptedObject = encrypt.encryptData();
-        
        
         
         return encryptedObject;
     }
     
-    public String decryptData(DataObject encryptedData) {
+    /**
+     * This method decrypts encrypted text passed as a DataObject
+     * argument by removing binary key to each byte of the data 
+     * @param DataObject encryptedData
+     * @return String decodedData
+     */
+    
+    public String decrypt(DataObject encryptedData) {
         
         Decrypt decrypt = new Decrypt(encryptedData);
         
@@ -40,5 +67,5 @@ public class XOR {
         
     }
         
+        
     }
-    
