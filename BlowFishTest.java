c5d33aed4b1c02bfba1344e9a37b5eac5b18ee39
okozlova <lasignorafortuna@gmail.com>
2016-02-14 10:38:03 -0500

    JUnit test for BlowFish. Updated on 02/14

--- BlowFishTest.java
+++ BlowFishTest.java
@@ -11,28 +11,44 @@ import org.junit.Test;
  * @date Updated on 02/09/2016
  **/
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This JUnit test case tests methods of class BlowFish
+ * 
+ * @author Olga Kazlova
+ * @project Bank Encryption Application
+ * @course CSMC 495
+ * @date 02/14/2016
+ **/
+
 public class BlowFishTest {
 
-	BlowFish blF = new BlowFish();
+	BlowFish fish = new BlowFish();
 
 	@Test
 	public void testEncrypt() {
-		String text1 = "Blowfish Algorithm";
+		String text1 = "Test case 1";
 		String text2 = "Test case 2";
-		String text3 = "Somithing random";
-		//test case 1
+		String text3 = "Test case 3";
+		// test case 1
 		System.out.println("\nText before Encryption: " + text1);
-		String cipherText1 = blF.encrypt(text1);
-		System.out.println("Cipher Text: " + cipherText1);
-		//test case 2
+		DataObject cipherText1 = fish.encrypt(text1);
+		String plainText1 = Base64.encode(cipherText1.encryptedData);
+		System.out.println("Cipher Text: " + plainText1);
+		// test case 2
 		System.out.println("\nText before Encryption: " + text2);
-		String cipherText2 = blF.encrypt(text2);
-		System.out.println("Cipher Text: " + cipherText2);
-		//test case 3
+		DataObject cipherText2 = fish.encrypt(text2);
+		String plainText2 = Base64.encode(cipherText2.encryptedData);
+		System.out.println("Cipher Text: " + plainText2);
+		// test case 3
 		System.out.println("\nText before Encryption: " + text3);
-		String cipherText3 = blF.encrypt(text3);
-		System.out.println("Cipher Text: " + cipherText3);
-		
+		DataObject cipherText3 = fish.encrypt(text3);
+		String plainText3 = Base64.encode(cipherText3.encryptedData);
+		System.out.println("Cipher Text: " + plainText3);
+
 		assertFalse(text1.equals(cipherText1));
 		assertFalse(text2.equals(cipherText2));
 		assertFalse(text3.equals(cipherText3));
@@ -40,35 +56,33 @@ public class BlowFishTest {
 
 	@Test
 	public void testDecrypt() {
-		//test case 1
-		String text1 = "Blowfish Algorithm";
-		String cipherText1 = blF.encrypt(text1);
-		System.out.println("\nCipher Text: " + cipherText1);
-		String decr1 = blF.decrypt(cipherText1);
-		System.out.println("Text after Decryption: "
-				+ decr1);
-		
-		//test case 2
+		// test case 1
+		String text1 = "Test case 1";
+		DataObject cipherText1 = fish.encrypt(text1);
+		String plainText1 = Base64.encode(cipherText1.encryptedData);
+		System.out.println("\nCipher Text: " + plainText1);
+		String decr1 = fish.decrypt(cipherText1);
+		System.out.println("Text after Decryption: " + decr1);
+
+		// test case 2
 		String text2 = "Test case 2";
-		String cipherText2 = blF.encrypt(text2);
-		System.out.println("\nCipher Text: " + cipherText2);
-		String decr2 = blF.decrypt(cipherText2);
-		System.out.println("Text after Decryption: "
-				+ decr2);
-		
-		//test case 3
-		String text3 = "Somithing random";
-		String cipherText3 = blF.encrypt(text3);
-		System.out.println("\nCipher Text: " + cipherText3);
-		String decr3 = blF.decrypt(cipherText3);
-		System.out.println("Text after Decryption: "
-				+ decr1);
-		
+		DataObject cipherText2 = fish.encrypt(text2);
+		String plainText2 = Base64.encode(cipherText2.encryptedData);
+		System.out.println("\nCipher Text: " + plainText2);
+		String decr2 = fish.decrypt(cipherText2);
+		System.out.println("Text after Decryption: " + decr2);
+
+		// test case 3
+		String text3 = "Test case 3";
+		DataObject cipherText3 = fish.encrypt(text3);
+		String plainText3 = Base64.encode(cipherText3.encryptedData);
+		System.out.println("\nCipher Text: " + plainText3);
+		String decr3 = fish.decrypt(cipherText3);
+		System.out.println("Text after Decryption: " + decr1);
+
 		assertTrue(text1.equals(decr1));
 		assertTrue(text2.equals(decr2));
 		assertTrue(text3.equals(decr3));
 	}
-
-	
 
 }
