af0ba3f917fe9986071b1a7c93854c255fe3be49
tlj1899 <tlj1899@gmail.com>
2016-02-21 10:11:48 -0500

    Changed package to data

--- DataObject.java
+++ DataObject.java
@@ -3,114 +3,121 @@ package data;
 import javax.crypto.SecretKey;
 
 /**
- * object to store encrypted data and encryption key to be send between
- * communication parties to enable encrypted communications
+ * object to store encrypted data and encryption key
+ * to be send between communication parties to enable encrypted communications
  * 
  * @author Jonathan Wojack
  * @project Bank Encryption Application
  * @course CSMC 495
- * @updated on 02/19/2016 by Jonathan Wojack
- * @updated on 02/21/2016 by Olga Kazlova (added getSecretKey(),
- *          getEncryptionAlgorihtm(), getKey(), and getEncryptedData())
+ * @updated on 2/20/2016 by Jonathan Wojack
+ * @updated on 2/21/2016 by Jonathan Wojack
  * 
+ * Changes:
+ * 
+ *  Changed package to data
  * 
  */
+
 public class DataObject implements java.io.Serializable {
-
-	byte[] encryptedData;
-	int[] key; // for XOR
-	SecretKey secretKey; // for AES and Blowfish
-
-	// encryption algorithm identifier
-	// 1 = AES
-	// 2 = BlowFish
-	// 3 = XOR
-
-	int encryptionAlgorithm;
-
-	// constructor for XOR algorithm, since it does not store key as SecretKey
-
-	public DataObject(byte[] encryptedData, int[] key,
-			String encryptionAlgorithm) {
-
-		this.encryptedData = encryptedData;
-		this.key = key;
-		this.encryptionAlgorithm = getEncryptionAlgorithmDesignator(encryptionAlgorithm);
-
-	}
-
-	// constructor for AES and Blowfish algorithms
-
-	public DataObject(byte[] encryptedData, SecretKey secretKey,
-			String encryptionAlgorithm) {
-
-		this.encryptedData = encryptedData;
-		this.secretKey = secretKey;
-		this.encryptionAlgorithm = getEncryptionAlgorithmDesignator(encryptionAlgorithm);
-
-	}
-
-	public int getEncryptionAlgorithmDesignator(String encryptionAlgorithm) {
-
-		if (encryptionAlgorithm.startsWith("AES")) {
-
-			return 1;
-
-		}
-
-		else if (encryptionAlgorithm.startsWith("BlowFish")) {
-
-			return 2;
-
-		}
-
-		else if (encryptionAlgorithm.startsWith("XOR")) {
-
-			return 3;
-
-		}
-
-		// invalid algorithm
-
-		return -1;
-
-	}
-
-	/**
-	 * getter for secretKey for encryption
-	 * 
-	 * @return secretKey
-	 */
-	public SecretKey getSecretKey() {
-		return secretKey;
-	}
-
-	/**
-	 * getter for encryptedData variable
-	 * 
-	 * @return encryptedData
-	 */
-	public byte[] getEncryptedData() {
-		return encryptedData;
-	}
-
-	/**
-	 * getter for type of encryptionAlgorithm
-	 * 
-	 * @return encryptionAlgorythm int
-	 */
-	public int getEncryptionAlgorithm() {
-		return encryptionAlgorithm;
-	}
-
-	/**
-	 * getter for encryption key
-	 * 
-	 * @return key int[]
-	 */
-	public int[] getKey() {
-		return key;
-
-	}
-
+    
+    byte[] encryptedData;
+    int[]key;               // for XOR
+    SecretKey secretKey;    // for AES and Blowfish
+    
+    // encryption algorithm identifier
+    // 1 = AES
+    // 2 = BlowFish
+    // 3 = XOR
+    
+    int encryptionAlgorithm;
+    
+    /**
+     * constructor for XOR algorithm, since it does not store key as SecretKey
+     * 
+     * @param encryptedData
+     * @param key
+     * @param encryptionAlgorithm 
+     */
+    
+    public DataObject(byte[] encryptedData, int[] key, String encryptionAlgorithm) {
+        
+        this.encryptedData = encryptedData;
+        this.key = key;
+        this.encryptionAlgorithm = getEncryptionAlgorithmDesignator(encryptionAlgorithm);
+        
+    }
+    
+    /**
+     * 
+     * constructor for AES and Blowfish algorithms
+     * @param encryptedData
+     * @param secretKey
+     * @param encryptionAlgorithm 
+     */
+    
+    public DataObject(byte[] encryptedData, SecretKey secretKey, String encryptionAlgorithm) {
+        
+        this.encryptedData = encryptedData;
+        this.secretKey = secretKey;
+        this.encryptionAlgorithm = getEncryptionAlgorithmDesignator(encryptionAlgorithm);
+        
+    }
+    
+    /**
+     * converts algorithm name into an integer
+     * 
+     * @param encryptionAlgorithm
+     * @return 
+     */
+    
+    public int getEncryptionAlgorithmDesignator(String encryptionAlgorithm) {
+        
+        if (encryptionAlgorithm.startsWith("AES")) {
+            
+            return 1;
+            
+        }
+        
+        else if (encryptionAlgorithm.startsWith("BlowFish")) {
+            
+            return 2;
+            
+        }
+        
+        else if (encryptionAlgorithm.startsWith("XOR")) {
+            
+            return 3;
+            
+        }
+        
+        // invalid algorithm
+        
+        return -1;
+        
+    }
+    
+    public SecretKey getSecretKey() {
+        
+        return this.secretKey;
+        
+    }
+    
+    public int[] getKey() {
+        
+        return this.key;
+        
+    }
+    
+    public byte[] getEncryptedData() {
+        
+        return this.encryptedData;
+        
+    }
+    
+    public int getEncryptionAlgorithm() {
+        
+        return this.encryptionAlgorithm;
+        
+    }
+    
 }
