94201ac78e7d5893bccd5ec4f197deb0eccc9695
tlj1899 <tlj1899@gmail.com>
2016-02-24 22:01:57 -0500

    Methods accept a variable quanity of String args

--- DAOInterface.java
+++ DAOInterface.java
@@ -3,140 +3,229 @@ package client;
 /**
  * Interface that includes all functionality of the BankingDAO
  * 
- * @author Grant
+ * @author Grant Sweeney
+ * @date 2/22/2016
+ * @course CMSC495
+ * @project Cryptography Banking Application
+ * @updated by Jonathan Wojack on 2/24/2016
+ * 
+ * Changes:
+ * 
+ * Changed all methods to accept a variable number of arguments to permit
+ * returning an error message and not terminating the application
+ * if an incorrect quantity is passed to the corresponding method
+ * 
+ * Each method stores its parameters as String[]
+ * 
  */
 
-/*
- * Project: CryptographyBankingApplication
- * Course: CMSC 495
- * Date: 02/22/2016
- */
+// interface
+
 public interface DAOInterface {
+    
     /**
+
      * Sends log in information to be checked against database records
      * 
-     * @param username Entered Username to be checked
-     * @param password Entered Password to be checked
+     * @param data
+     * 
      * @return User type (customer or employee) if valid combination, denied if invalid
      */
-    public String confirmLogIn(String username, String password);
+   
+    public String confirmLogIn(String...data);
+    
+        // method parameters:
+
+        // username
+        // password
     
     /**
-     * Requests information needed for customer start screen
+     * get customer information needed for start screen
      * 
-     * @param username customer username
-     * @param password customer password
+     * @param data
+     * 
      * @return customer first name, last name, and all customer account information
      */
-    public String getCustomerScreenInfo(String username, String password);
+    
+    public String getCustomerScreenInfo(String...data);
+    
+        // method parameters:
+
+        // username
+        // password
     
     /**
-     * Request for account information
+     * get customer account information
      * 
-     * @param accountNumber account number being searched for
+     * @param data
+     * 
      * @return account information including account balance, type, and number
      */
-    public String getCustomerAccount(String accountNumber);
+    
+    public String getCustomerAccount(String...data);
+    
+        // method parameters:
+    
+        // accountNumber
     
     /**
      * Request for all customer information
      * 
-     * @param firstName customer first name used for search
-     * @param lastName customer last name used for search
+     * @param data
+     * 
      * @return all customer information including name, address, phone number, and account info
      */
-    public String getCustomerInformation(String firstName, String lastName);
+
+    public String getCustomerInformation(String...data);
+    
+        // method parameters:
+    
+        // firstName
+        // lastname
     
     /**
-     * Reqest to create a new user
+     * Request to create a new user
      * 
-     * @param username entered username
-     * @param password entered password
-     * @param employeeStatus yes if employee checkbox is checked
-     * @param firstName entered first name
-     * @param lastName entered last name
-     * @param address entered address
-     * @param city entered city
-     * @param state entered state
-     * @param zipCode entered zip code
-     * @param phoneNumber entered phone number
+     * @param data
+     * 
      * @return "confirm" if created, "username taken" or "profile exists" if these errors occur
      */
-    public String createUserProfile(String username, String password, String employeeStatus,
-                                    String firstName, String lastName, String address, String city,
-                                    String state, String zipCode, String phoneNumber);
+
+    public String createUserProfile(String...data);
+        
+        // method parameters:
+
+        // username
+        // password
+        // employeeStatus
+        // firstName
+        // lastName
+        // address
+        // city
+        // state
+        // zipCode
+        // phoneNumber
     
     /**
      * Request to create a new bank account
      * 
-     * @param accountType string containing the selected combobox type
-     * @param accountBalance balance for the new account
-     * @param date date the account is create
-     * @return string including new account number and all entered information, "error" if error occurs
+     * @param data
+     * 
+     * @return String including new account number and all entered information, "error" if error occurs
      */
-    public String createNewAccount(String accountType, String accountBalance, String date);
+    
+    public String createNewAccount(String...data);
+        
+        // method parameters:
+        
+        // accountType
+        // accountBalance
+        // date
     
     /**
      * Request to delete a user profile
      * 
-     * @param firstName name used to search for profile
-     * @param lastName name used to search for profile
+     * @param data
+     * 
      * @return "confirm" if deleted, "error" if error occurs
      */
-    public String deleteUserProfile(String firstName, String lastName);
+    
+    public String deleteUserProfile(String...data);
+    
+        // method parameters:
+    
+        // firstName
+        // lastName
     
     /**
      * Request to delete bank account
      * 
-     * @param accountNumber account number used to search for account
+     * @param data
+     * 
      * @return "confirm" if deleted, "error" if error occurs
      */
-    public String deleteAccount(String accountNumber);
+    
+    public String deleteAccount(String...data);
+    
+        // method parameters:
+
+        // accountNumber
     
     /**
      * Request for the transaction history of a bank account
      * 
-     * @param accountNumber account number used to search for account
+     * @param data
+     * 
      * @return transaction history, including all fields to populate transaction screen
+     *         comma-separated fields, with newlines between transactions
      */
-    public String getTransactionHistory(String accountNumber);
+    
+    public String getTransactionHistory(String...data);
+    
+        // method parameters:
+
+        // accountNumber
     
     /**
      * Request to deposit money in selected account
      * 
-     * @param accountNumber account to deposit money
-     * @param amount amount to deposit
+     * @param data
+     * 
      * @return new account balance if successful, "error" if error occurs
      */
-    public String depositMoney(String accountNumber, String amount);
+    
+    public String depositMoney(String...data);
+    
+        // method parameters:
+
+        // accountNumber
+        // amount
     
     /**
      * Request to withdraw money from selected account
      * 
-     * @param accountNumber account to withdraw money from
-     * @param amount amount to withdraw
+     * @param data
+     *
      * @return new account balance if successful, error type if error occurs
      */
-    public String withdrawMoney(String accountNumber, String amount);
+    
+    public String withdrawMoney(String...data);
+    
+        // method parameters:
+
+        // accountNumber
+        // amount
     
     /**
      * Request to transfer money between two accounts
      * 
-     * @param accountFrom account money is transfered from
-     * @param amount amount to transfer
-     * @param accountTo account money is transfered to
+     * @param data
+     * 
      * @return new account balance for both accounts if successful, error type if error occurs
      */
-    public String transferMoney(String accountFrom, String amount, String accountTo);
+    
+    public String transferMoney(String...data);
+    
+        // method parameters:
+
+        // accountFrom
+        // amount
+        // accountTo
     
     /**
      * Request to pay mortgage from a bank account
      * 
-     * @param accountFrom account money is taken from
-     * @param amount amount to pay
-     * @param mortgageAccount mortgage to be payed
+     * @param data
+     * 
      * @return new account balance for both accounts if successful, error type if error occurs
      */
-    public String payMortgage(String accountFrom, String amount, String mortgageAccount);
     
-} //end interface
+    public String payMortgage(String...data);
+    
+        // method parameters:
+
+        // accountFrom
+        // amount
+        // mortgageAccount
+    
+} // end interface
